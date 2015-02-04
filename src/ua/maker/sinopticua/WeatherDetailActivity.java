2cd4165dfb5b8e50195038cc8cf64805f782474b
Daniil Pavenko <daniil.pavenko@gmail.com>
2015-02-05 00:57:50 +0200

    add msg for empty detail

--- src/ua/maker/sinopticua/WeatherDetailActivity.java
+++ src/ua/maker/sinopticua/WeatherDetailActivity.java
@@ -5,13 +5,18 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import ua.maker.sinopticua.adapters.WeatherItemDetailPagerAdapter;
 import ua.maker.sinopticua.constants.App;
@@ -24,12 +29,14 @@ import ua.setcom.widgets.view.SlidingTabLayout;
 public class WeatherDetailActivity extends ActionBarActivity {
 	
 	private static final String TAG = "WeatherDetailActivity";
+    private static final int SECOND_TO_RESTART = 5;
 	
 	private ItemWeather weather;
     private ViewPager viewPager;
     private WeatherItemDetailPagerAdapter weatherPagerAdapter;
     private SlidingTabLayout tabSlidingTitle;
     private ProgressBar pb;
+    private TextView tvMessage;
 
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +47,7 @@ public class WeatherDetailActivity extends ActionBarActivity {
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         tabSlidingTitle = (SlidingTabLayout) findViewById(R.id.title_tab);
         pb = (ProgressBar) findViewById(R.id.pb_load);
+        tvMessage = (TextView) findViewById(R.id.tv_message);
         if(weather != null && putHttp(weather))
             prepareLoadData();
     }
@@ -51,7 +59,9 @@ public class WeatherDetailActivity extends ActionBarActivity {
 
     private boolean putHttp(ItemWeather weather) {
         try {
-            weather.urlDetail = "http:" + weather.urlDetail;
+            if (!Patterns.WEB_URL.matcher(weather.urlDetail).matches()) {
+                weather.urlDetail = "https:" + weather.urlDetail;
+            }
             return true;
         } catch (Exception e) {
             return false;
@@ -60,19 +70,63 @@ public class WeatherDetailActivity extends ActionBarActivity {
 
     private void prepareLoadData() {
         enablePb(true);
+        enableMsg(false, getString(R.string.err_downloading_detail_info), SECOND_TO_RESTART);
         try {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    Document document = Jsoup.parse(Tools.getWebPage(weather.urlDetail));
-                    ArrayList<ItemDetail> listItemsDetail = DataParser.parseDetailInfo(document);
-                    setupData(listItemsDetail);
-                    enablePb(false);
+                    try {
+                        String page = Tools.getWebPage(weather.urlDetail);
+                        Document document = Jsoup.parse(page);
+                        ArrayList<ItemDetail> listItemsDetail = DataParser.parseDetailInfo(document);
+                        setupData(listItemsDetail);
+                        enablePb(false);
+                    } catch (Exception e) {
+                        Log.e(TAG, "CRASH #### " + e.getLocalizedMessage());
+                        enableMsg(true, getString(R.string.err_downloading_detail_info), SECOND_TO_RESTART);
+                        enablePb(false);
+                    }
                 }
             }).start();
         } catch (Exception e) {
             enablePb(false);
         }
+    }
+
+    private void enableMsg(final boolean enable, final String msg, final int secondsForRestart) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (msg != null) {
+                    if (enable) {
+                        tvMessage.setVisibility(TextView.VISIBLE);
+                        if (secondsForRestart == -1) {
+                            tvMessage.setText(msg);
+                        } else {
+                            new Timer().schedule(new TimerTask() {
+                                int currentSecond = secondsForRestart;
+                                @Override
+                                public void run() {
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            tvMessage.setText(String.format(msg, currentSecond));
+                                        }
+                                    });
+                                    currentSecond--;
+                                    if (currentSecond == -1) {
+                                        prepareLoadData();
+                                        cancel();
+                                    }
+                                }
+                            }, 0, 1000);
+                        }
+                    } else {
+                        tvMessage.setVisibility(TextView.GONE);
+                    }
+                }
+            }
+        });
     }
 
     private void enablePb(final boolean enable) {
